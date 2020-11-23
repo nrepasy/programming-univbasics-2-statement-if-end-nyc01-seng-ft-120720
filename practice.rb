79bfaec4c7d7a6c555b14802bc7dc0c39d928c65
nick repasy <nrepasy@gmail.com>
2020-11-23 01:28:14 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,8 +1,4 @@
-chance_of_rain = 1000	
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-  elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75 )
-  puts "PAck an unbrella!"
-else
-  puts "Stay home and read Hegel"
-end
+puts "You know wat year it is?"
+this_year = 2019
+puts "Hey, it's 2019!" if this_year == 2019
+
